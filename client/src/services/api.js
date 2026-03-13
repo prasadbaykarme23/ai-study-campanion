e30913f2eebcea034b02f7497029705ab6238577
premchorge <premchorge067@gmail.com>
2026-03-13 11:27:24 +0530

    abc

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-const API_BASE = API_URL || 'http://localhost:5000/api';
+const API_BASE = API_URL || (process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api');
 const API_ORIGIN = API_BASE.replace(/\/api\/?$/, '');
 
 const api = axios.create({
